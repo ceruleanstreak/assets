650d4fa61874daf8bdf4a9e670e5693480619f24
Ilya <neuralquantum@gmail.com>
2022-03-06 17:34:39 +0300

    Added events

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -75,9 +75,27 @@ pub mod pallet {
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		SomethingStored(u32, T::AccountId),
+        /// A new asset was successfully created.
+        AssetCreated { asset_id: u32, owner: T::AccountId },
+        /// The price of an asset was successfully set.
+        AssetPriceSet {
+            asset_id: u32,
+            price: Option<BalanceOf<T>>,
+        },
+        /// An asset was successfully transferred.
+        AssetTransferred {
+            from: T::AccountId,
+            to: T::AccountId,
+            asset_id: u32,
+        },
+        /// An asset was successfully sold.
+        AssetSold {
+            seller: T::AccountId,
+            buyer: T::AccountId,
+            asset_id: u32,
+            price: BalanceOf<T>,
+        },
+
 	}
 
 	// Errors inform users that something went wrong.
@@ -118,23 +136,6 @@ pub mod pallet {
 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/v3/runtime/origins
-			let who = ensure_signed(origin)?;
-
-			// Update storage.
-			<Something<T>>::put(something);
-
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored(something, who));
-			// Return a successful DispatchResultWithPostInfo
-			Ok(())
-		}
 
 
 	}
