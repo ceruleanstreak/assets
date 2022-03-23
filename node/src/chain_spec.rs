2ce36f9e99105ecfdd179465f9b599f911f782e9
ilya <neuralquantum@gmail.com>
2022-03-23 13:18:21 +0300

    renamed to planet assets

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -1,6 +1,6 @@
 use node_template_runtime::{
 	AccountId, AuraConfig, BalancesConfig, GenesisConfig, GrandpaConfig, Signature, SudoConfig, //
-	SystemConfig, TemplateModuleConfig, WASM_BINARY,
+	SystemConfig, PlanetAssetsConfig, WASM_BINARY,
 };
 use sc_service::ChainType;
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
@@ -154,6 +154,6 @@ fn testnet_genesis(
 			key: Some(root_key),
 		},
 		transaction_payment: Default::default(),
-		template_module: TemplateModuleConfig { assets: vec![] },
+		planet_assets: PlanetAssetsConfig { assets: vec![] },
 	}
 }
